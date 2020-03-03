025669484d9d815503cf4ec8ff9429c761f79118
leewalton2014 <35839824+leewalton2014@users.noreply.github.com>
2020-03-03 23:58:44 +0000

    setup php scripts and functions file

---
+++ project/holidays.php
@@ -0,0 +1,12 @@
+<?php
+include 'functions.php';
+setSessionPath();
+startHTML('Travel Co.','Home of travel for a newcastle travel agent');
+makeHeader();
+
+echo "";
+
+
+makeFooter();
+endHTML();
+?>
