a8f281fc73cf567c3c230e69fcab1e6f790e9027
Lee Walton <lee.walton2014@outlook.com>
2020-03-26 15:39:50 +0000

    updated layout

--- project/holidays.php
+++ project/holidays.php
@@ -4,13 +4,12 @@ setSessionPath();
 startHTML('Travel Co.','Home of travel for a newcastle travel agent');
 makeHeader();
 
-echo getcwd();
+//echo getcwd();
 
 
 
-$getUsersQuery = "SELECT tc_hotels.hotelID, roomID, nightRatePerPerson, hotelName, hotelDescription, locationCity, locationCountry, imageRef
-FROM tc_rooms INNER JOIN tc_roomtype ON tc_rooms.typeID = tc_roomtype.typeID
-INNER JOIN tc_hotels ON tc_rooms.hotelID = tc_hotels.hotelID
+$getUsersQuery = "SELECT tc_hotels.hotelID, roomID, hotelName, hotelDescription, locationCity, locationCountry, imageRef
+FROM tc_hotels INNER JOIN tc_rooms ON tc_hotels.hotelID = tc_rooms.hotelID
 INNER JOIN tc_locations ON tc_hotels.hotelLocation = tc_locations.locationID";
 
 
@@ -24,20 +23,20 @@ echo "<div class='widthWrap splitCol'>";
 while ($rowObj = $queryResult->fetchObject()){
     echo "<div class='smallHoliday'>
         <img src='{$rowObj->imageRef}'/>
-        
-        <h1><a href='hotel.php?hotelID={$rowObj->hotelID}'>{$rowObj->hotelName}</a></h1> 
+
+        <a href='hotel.php?hotelID={$rowObj->hotelID}'><h1>{$rowObj->hotelName}</h1></a>
         <h2>{$rowObj->locationCity}, {$rowObj->locationCountry}</h2>
-        
+
         <div class='splitCol'>
           <div class='rating'><img src='img/ratingPlaceholder.jpg'/></div>
-          <span class='price'><p>From</p><h2>£{$rowObj->nightRatePerPerson}pp</h2></span>
+          <span class='price'><p>From</p><h2>£(min price)pp</h2></span>
         </div>
       </div>";
 }
 
 //<a href='editRecord.php?recordID={$rowObj->recordID}'>{$rowObj->recordTitle}</a>
 
-holidaysJs();
+//holidaysJs();
 
 
 echo "</div>";
--- project/hotel.php
+++ project/hotel.php
@@ -27,25 +27,25 @@ $queryResult = $dbConn->query($getUsersQuery);
 
 
 echo "<div class='widthWrap splitCol'>";
-
+/*
 while ($rowObj = $queryResult->fetchObject()){
     echo "<div class='smallHoliday'>
         <img src='{$rowObj->imageRef}'/>
         <h1>{$rowObj->hotelName}</h1>
-        
+
         <div class='splitCol'>
         <div class='rating'><img src='img/ratingPlaceholder.jpg'/></div>
         <span class='price'><p>From</p><h2>£{$rowObj->nightRatePerPerson}pp</h2></span>
         </div>
-        
-        
+
+
         <h2>{$rowObj->locationCity}, {$rowObj->locationCountry}</h2>
-        
+
         <p>{$rowObj->hotelDescription}</p>
         <h2>Board Type</h2>
         <p>{$rowObj->boardType}</p>
-        
-        
+
+
         <form action='hotelBooking.php' method='post'>
         <h2>Select Room Type</h2>
         <select>
@@ -55,25 +55,69 @@ while ($rowObj = $queryResult->fetchObject()){
             <input type='submit' value='Book Now'>
         </form>
         <hr>
-        
-        
-        <h2>Read the Reviews</h2>
-        
-        
-          
-          
-          
-          
-          
-       
-      </div>
-      
-      
-      ";
-    
-    
-}
 
+
+        <h2>Read the Reviews</h2>
+
+
+
+
+
+
+
+
+      </div>
+
+
+      ";
+
+
+}*/
+while ($rowObj = $queryResult->fetchObject()){
+    echo "<div class='widthWrap splitCol'>
+      <div class='hotel'>
+        <p>Hotels > {$rowObj->locationCountry} > {$rowObj->locationCity} > <b>{$rowObj->hotelName}</b></p>
+        <h1>{$rowObj->hotelName}</h1>
+        <h2>{$rowObj->locationCity}, {$rowObj->locationCountry}</h2>
+        <div class='splitCol'>
+          <p>Star rating</p>
+          <span class='price'><p>From</p><h2>425pp</h2></span>
+        </div>
+        <img src='{$rowObj->imageRef}'/>
+
+        <article>
+          <h1>Holiday Description</h1>
+          <div>
+            <p>{$rowObj->hotelDescription}</p>
+            <ul>
+              <li>List of items example</li>
+              <li>List of items example</li>
+              <li>List of items example</li>
+              <li>List of items example</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+          </div>
+          <h1>Location</h1>
+          <div>
+            <p>
+            Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet
+            purus. Vivamus hendrerit, dolor at aliquet laoreet.<br>
+            </p>
+            <iframe src='https://www.google.com/maps/embed/v1/place?key=AIzaSyD-Ikb9FiJVmyXVjSUH2Wms2ot8enQbzu0&q={$rowObj->locationCity},{$rowObj->locationCountry}' width='100%' height='400' frameborder='0' style='border:0;' allowfullscreen=''></iframe>
+          </div>
+        </article>
+
+      </div>
+      <aside>
+        <div class='cAlign hotelContinue'>
+          <span class='price'>&#163;<h2>425pp</h2></span>
+          <br>
+          <span class='price'><p>Total Price &#163;</p><h2>850</h2></span>
+          <input class='viewBtn' type='submit' value='Continue'>
+        </div>
+        </aside>
+      </div>";
+}
 
 
 
